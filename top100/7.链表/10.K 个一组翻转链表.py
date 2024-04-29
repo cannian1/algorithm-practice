c207dcc0ca8e3cfdec0ab800a51987cb6f198ea8
cya <2081215119@qq.com>
2024-04-29 17:01:55 +0800

    ,

--- top100/7.链表/10.K 个一组翻转链表.py
+++ top100/7.链表/10.K 个一组翻转链表.py
@@ -37,3 +37,51 @@ class Solution:
             p0.next = pre
             p0 = nxt
         return dummy.next
+
+
+class ListNode2:
+    """官解"""
+    """翻转一个子链表，并且返回新的头与尾"""
+
+    def reverse(self, head: ListNode, tail: ListNode):
+        prev = tail.next
+        p = head
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+            prev = p
+            p = nex
+        return tail, head
+
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        hair = ListNode(0)
+        hair.next = head
+        pre = hair
+
+        while head:
+            tail = pre
+            # 查看剩余部分长度是否大于等于 k
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return hair.next
+            nex = tail.next
+            head, tail = self.reverse(head, tail)
+            # 把子链表重新接回原链表
+            pre.next = head
+            tail.next = nex
+            pre = tail
+            head = tail.next
+
+        return hair.next
+
+
+head = ListNode(val=1,
+                next=ListNode(val=2, next=ListNode(val=3, next=ListNode(val=4, next=ListNode(val=5, next=ListNode())))))
+s = Solution()
+s.reverseKGroup(head, 2)
+res = []
+while head:
+    res.append(head.val)
+    head = head.next
+print(res)
