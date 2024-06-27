457d4f50383837315a0828b135fd06f57f3a8659
Clauvio <2081215119@qq.com>
2024-06-27 23:58:50 +0800

    .

--- top100/14.贪心/1.买卖股票的最佳时机.py
+++ top100/14.贪心/1.买卖股票的最佳时机.py
@@ -6,8 +6,8 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         ans = 0
-        min_pirce = prices[0]  # min_pirce 维护的是 prices[i] 左侧元素的最小值
+        min_price = prices[0]  # min_pirce 维护的是 prices[i] 左侧元素的最小值
         for p in prices:
-            ans = max(ans, p - min_pirce)  # 由于只能买卖一次，所以在遍历中，维护 prices[i] - min_pirce 的最大值就是答案
-            min_pirce = min(min_pirce, p)
+            ans = max(ans, p - min_price)  # 由于只能买卖一次，所以在遍历中，维护 prices[i] - min_pirce 的最大值就是答案
+            min_price = min(min_price, p)
         return ans
