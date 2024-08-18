2d2e1353fc11bac15e52632e5070871e3d8a8a47
Clauvio <2081215119@qq.com>
2024-08-19 00:47:21 +0800

    .

--- go_version/top100/7.linklist/8.remove-nth-node-from-end-of-list.go
+++ go_version/top100/7.linklist/8.remove-nth-node-from-end-of-list.go
@@ -6,7 +6,8 @@ package linklist
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	fast := head
 	slow := &ListNode{Next: head}
-	dummy := slow
+	// 必须要有 dummy 节点，否则当链表中节点数等于 n 时，将会把头节点删掉
+	dummy := slow // dummy 可以维护新的头节点的关系
 
 	for _ = range n {
 		fast = fast.Next
