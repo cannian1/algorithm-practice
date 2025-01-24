d829e4d3ce462d856dda421f4d80f66e557fe2f1
Clauvio <2081215119@qq.com>
2025-01-24 20:49:58 +0800

    feat(top100): 二叉树

--- go_version/top100/8.binary_tree/14.lowest-common-ancestor-of-a-binary-tree.go
+++ go_version/top100/8.binary_tree/14.lowest-common-ancestor-of-a-binary-tree.go
@@ -17,3 +17,16 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	}
 	return root // 左右都找到，当前节点是最近公共祖先
 }
+
+// 拓展
+// 235. 二叉搜索树的最近公共祖先
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/
+func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
+	if p.Val < root.Val && q.Val < root.Val { // p 和 q 都在左子树
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if p.Val > root.Val && q.Val > root.Val { // p 和 q 都在右子树
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root // 其它
+}
